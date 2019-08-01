a466b175f1a682b69c8c6a3e531521969178fcab
pe.ragetly <pe.ragetly@gmail.com>
2019-08-01 20:45:20 +0200

    a python file has been included, this file will gather all functions

---
+++ Project_2.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+"""Contient toutes les fonctions
+qui seront utilisées dans le projet 2."""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
