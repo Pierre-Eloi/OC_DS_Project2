3814c736336f25645242561bf876f12a3c834633
pe.ragetly <pe.ragetly@gmail.com>
2019-11-13 19:03:00 +0100

    Ajout de l'exploration des données.

--- analysis/datavisu.py
+++ analysis/datavisu.py
@@ -24,24 +24,24 @@ def nice_plot(plot, legend=False, title="", subtitle="", file=""):
     axes : Matplotlib axis object
     """
     # Set axis
-    plot.tick_params(axis="both", which="both", labelsize=10, colors="#333333")
     x_min, x_max = plot.get_xlim()
     y_min, y_max = plot.get_ylim()
     x_label = plot.get_xlabel()
     y_label = plot.get_ylabel()
-    plot.set_xlabel(x_label, color="#333333", weight="bold", fontsize=10)  
-    plot.set_ylabel(y_label, color="#333333", weight="bold", fontsize=10)  
+    plot.set_xlabel(x_label, weight="bold")  
+    plot.set_ylabel(y_label, weight="bold")  
     # Add a title and a subtitle
     title_x = x_min - (x_max-x_min)*.05
-    title_y = y_max + (y_max-y_min)*.13
-    subtitle_y = y_max + (y_max-y_min)*.07
-    plot.text(x=title_x, y=title_y, s=title, fontsize=15,
-                weight="bold", color="#333333")
-    plot.text(x=title_x, y=subtitle_y, s=subtitle, fontsize=10,
-                color="#333333")    
+    title_y = y_max + (y_max-y_min)*.1
+    subtitle_y = y_max + (y_max-y_min)*.05
+    plot.text(x=title_x, y=title_y, s=title, fontsize=14,
+                weight="bold")
+    plot.text(x=title_x, y=subtitle_y, s=subtitle)
+    # Add a grid
+    plt.grid(linestyle='dashed')
     # Add a legend
     if legend:
-        plt.legend(loc='best', fontsize=10, facecolor='w', edgecolor='#333333')
+        plt.legend(loc='best', facecolor='w', edgecolor='b')
     # Save the plot
     plt.savefig(file + ".png", bbox_inches = "tight")
     plt.show()
